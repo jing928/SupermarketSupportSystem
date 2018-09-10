939be1397a4b86841678ed88c7c1e16e4030e100
Jing Li <lyovse@gmail.com>
2018-09-10 12:18:22 +1000

    Format Manager

--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -4,37 +4,39 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Manager extends Employee {
-private SimpleDateFormat salesReportDate=new SimpleDateFormat("MM-DD-YYYY");
+	private SimpleDateFormat salesReportDate = new SimpleDateFormat("MM-DD-YYYY");
 
-public Manager() {
-	super();
-}
-
-public boolean overridePrice(Product product, double price) {
-	if(price<0) {
-		return false;
+	public Manager() {
+		super();
 	}
-		
-	return true;
-}
 
-public boolean modifyPromotion(Product product, double bulkQuantity,double discount) {
-	if(bulkQuantity<=0 || discount<=0||discount>=1) {
-		return false;
+	public boolean overridePrice(Product product, double price) {
+		if (price < 0) {
+			return false;
+		}
+
+		return true;
 	}
-		
-	return true;
-}
 
-public String viewSalesReport(Date startDate, Date endDate) {
-	return "The Start Date is:"+ salesReportDate.format(startDate)+"/nThe End Date is:"+salesReportDate.format(endDate);
-}
+	public boolean modifyPromotion(Product product, double bulkQuantity, double discount) {
+		if (bulkQuantity <= 0 || discount <= 0 || discount >= 1) {
+			return false;
+		}
 
-public String viewSupplyReport() {
-	return "haha";
-}
-public String viewMostProfitableItem() {
-	return "hehe";
-}
+		return true;
+	}
+
+	public String viewSalesReport(Date startDate, Date endDate) {
+		return "The Start Date is:" + salesReportDate.format(startDate) + "/nThe End Date is:"
+				+ salesReportDate.format(endDate);
+	}
+
+	public String viewSupplyReport() {
+		return "haha";
+	}
+
+	public String viewMostProfitableItem() {
+		return "hehe";
+	}
 
 }
