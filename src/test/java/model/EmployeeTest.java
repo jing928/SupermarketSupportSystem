8e4ae90b2c768ff06a44a5cb9934b746ac2bad0d
Jing Li <lyovse@gmail.com>
2018-09-10 12:14:41 +1000

    Add EmployeeTest

---
+++ src/test/java/model/EmployeeTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@Test
+	void testGetEmBirthday() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
