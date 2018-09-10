42aa3535b8618b5b6ca5898a63edfb08a7ab245f
Jing Li <lyovse@gmail.com>
2018-09-10 12:24:46 +1000

    Format SalesLineItem

--- src/test/java/model/SalesLineItemTest.java
+++ src/test/java/model/SalesLineItemTest.java
@@ -10,57 +10,44 @@ public class SalesLineItemTest {
 	private String itemName = "apple";
 	private double quantity = 2.5;
 	private double price = 10;
-    private SalesLineItem saleslineitem;
-    
-    @BeforeEach
-	public void setUp() throws Exception {
-    	saleslineitem = new SalesLineItem(itemName, quantity, price, item);
-    }
-    
-    @Test
-    public void testSalesLineItem() {
-    	assertEquals(itemName, saleslineitem.getItemName());
-    	assertTrue(saleslineitem.getQuantity()==quantity);
-    	assertTrue(saleslineitem.getPrice()==price);
-    }
-	
-    
+	private SalesLineItem saleslineitem;
 
-    @Test
+	@BeforeEach
+	public void setUp() throws Exception {
+		saleslineitem = new SalesLineItem(itemName, quantity, price, item);
+	}
+
+	@Test
+	public void testSalesLineItem() {
+		assertEquals(itemName, saleslineitem.getItemName());
+		assertTrue(saleslineitem.getQuantity() == quantity);
+		assertTrue(saleslineitem.getPrice() == price);
+	}
+
+	@Test
 	public void testSetItem() {
-    	Product item2 = new Product("fgf", 453,true);
-    	saleslineitem.setItem(item2);
-    	assertTrue(saleslineitem.getItem().equals(item2));
-    	//assertEquals(item2,saleslineitem.getItem());
+		Product item2 = new Product("fgf", 453, true);
+		saleslineitem.setItem(item2);
+		assertTrue(saleslineitem.getItem().equals(item2));
+		// assertEquals(item2,saleslineitem.getItem());
 	}
-    
-    
-    
-    
-    @Test
+
+	@Test
 	public void testSetItemName() {
-    	saleslineitem.setItemName("orange");
-		assertEquals("orange",saleslineitem.getItemName());
+		saleslineitem.setItemName("orange");
+		assertEquals("orange", saleslineitem.getItemName());
 	}
-	
-    @Test
-    public void testSetQuantity() {
-    	saleslineitem.setQuantity(9.9);
-    	assertTrue(saleslineitem.getQuantity()==9.9);
-    }
-	
-    @Test
-    public void testSetPrice() {
-    	saleslineitem.setPrice(6.6);
-    	assertTrue(saleslineitem.getPrice()==6.6);
-    }
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void testSetQuantity() {
+		saleslineitem.setQuantity(9.9);
+		assertTrue(saleslineitem.getQuantity() == 9.9);
+	}
+
+	@Test
+	public void testSetPrice() {
+		saleslineitem.setPrice(6.6);
+		assertTrue(saleslineitem.getPrice() == 6.6);
+	}
+
 }
