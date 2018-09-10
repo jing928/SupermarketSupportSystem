7b06b2a046b5f26ea14390d45991cc499171ec70
Jing Li <lyovse@gmail.com>
2018-09-10 12:07:55 +1000

    Format Membership

--- src/main/java/model/Membership.java
+++ src/main/java/model/Membership.java
@@ -1,33 +1,32 @@
 package model;
 
-
 public class Membership {
 	private String cardNum;
 	private int pointBalance;
-	
+
 	public Membership(String cardNum) {
 		this.cardNum = cardNum;
 		this.pointBalance = 0;
 	}
-	
+
 	public String getCardNum() {
 		return cardNum;
 	}
-	
-	public Integer getPointBalance() {
+
+	public int getPointBalance() {
 		return pointBalance;
 	}
-	
-	public boolean earnPoints(int amount){
-		if(amount >= 0){
+
+	public boolean earnPoints(int amount) {
+		if (amount >= 0) {
 			pointBalance += amount;
 			return true;
 		}
 		return false;
 	}
-	
-	public boolean usePoints(int amount){
-		if(amount >= 0 && pointBalance >= amount){
+
+	public boolean usePoints(int amount) {
+		if (amount >= 0 && pointBalance >= amount) {
 			pointBalance -= amount;
 			return true;
 		}
