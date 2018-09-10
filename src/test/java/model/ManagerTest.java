63267224e3278fc8c50bb91a0f8521d96f2e21af
Jing Li <lyovse@gmail.com>
2018-09-10 12:17:47 +1000

    Use JUnit 5 for ManagerTest

--- src/test/java/model/ManagerTest.java
+++ src/test/java/model/ManagerTest.java
@@ -1,14 +1,15 @@
 package model;
 
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ManagerTest {
 	
 	Manager manager1;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		manager1=new Manager();
 	}
