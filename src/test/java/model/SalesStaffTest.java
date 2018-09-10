b8437a6c85cab278016c47442e45c2fc3a7bae79
Jing Li <lyovse@gmail.com>
2018-09-10 12:25:45 +1000

    Use JUnit 5 for SalesStaffTest

--- src/test/java/model/SalesStaffTest.java
+++ src/test/java/model/SalesStaffTest.java
@@ -1,15 +1,15 @@
 package model;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SalesStaffTest {
 	
 	SalesStaff salesStaff;
 	
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		salesStaff = new SalesStaff();
 	}
