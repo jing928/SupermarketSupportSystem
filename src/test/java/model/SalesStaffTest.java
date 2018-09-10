690b84e3041515bb24ef18e5f7020c290c7ce4f4
Jing Li <lyovse@gmail.com>
2018-09-10 12:27:27 +1000

    Format SalesStaff

--- src/test/java/model/SalesStaffTest.java
+++ src/test/java/model/SalesStaffTest.java
@@ -6,27 +6,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SalesStaffTest {
-	
+
 	SalesStaff salesStaff;
-	
+
 	@BeforeEach
 	public void setUp() throws Exception {
 		salesStaff = new SalesStaff();
 	}
+
 	@Test
 	public void testAddDebitCard() {
-		Customer customer1=new Customer("111","abc");
-		assertTrue(salesStaff.addDebitCard(customer1,11));
-		assertFalse(salesStaff.addDebitCard(customer1,-2));
+		Customer customer1 = new Customer("111", "abc");
+		assertTrue(salesStaff.addDebitCard(customer1, 11));
+		assertFalse(salesStaff.addDebitCard(customer1, -2));
 	}
 
 	@Test
 	public void testTopUpDebitCard() {
-		Customer customer1=new Customer("111","abc");
-		assertTrue(salesStaff.topUpDebitCard(customer1,11));
-		assertFalse(salesStaff.topUpDebitCard(customer1,-2));
+		Customer customer1 = new Customer("111", "abc");
+		assertTrue(salesStaff.topUpDebitCard(customer1, 11));
+		assertFalse(salesStaff.topUpDebitCard(customer1, -2));
 	}
-
-
 
 }
