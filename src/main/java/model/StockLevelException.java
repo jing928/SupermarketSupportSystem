05ea1976f87d76d0ad0006529a7530ffc13d9c4f
Jing Li <lyovse@gmail.com>
2018-09-19 00:11:29 +1000

    Add a new stock level exception

---
+++ src/main/java/model/StockLevelException.java
@@ -0,0 +1,9 @@
+package model;
+
+public class StockLevelException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public StockLevelException(String message) {
+		super(message);
+	}
+}
