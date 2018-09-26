7b9999cca714ddf4817f4c8615f711424acef816
Jing Li <lyovse@gmail.com>
2018-09-26 11:19:11 +1000

    Finish MainSystem model part

--- src/main/java/model/MainSystem.java
+++ src/main/java/model/MainSystem.java
@@ -1,7 +1,9 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainSystem implements Serializable {
@@ -15,6 +17,7 @@ public class MainSystem implements Serializable {
 	// here name is easier to remember.
 	private Map<String, String> barCodeLookUp;
 	private Map<String, Product> catalog;
+	private List<Sale> sales;
 
 	public MainSystem() {
 		this.employees = new HashMap<String, Employee>();
@@ -22,6 +25,31 @@ public class MainSystem implements Serializable {
 		this.customers = new HashMap<String, Customer>();
 		this.barCodeLookUp = new HashMap<String, String>();
 		this.catalog = new HashMap<String, Product>();
+		this.sales = new ArrayList<Sale>();
+	}
+
+	public void addEmployee(Employee employee) {
+		employees.put(employee.getId(), employee);
+	}
+
+	public void addSupplier(Supplier supplier) {
+		suppliers.put(supplier.getId(), supplier);
+	}
+
+	public void addCustomer(Customer customer) {
+		customers.put(customer.getId(), customer);
+	}
+
+	public void addNameBarCodePair(String productName, String productBarCode) {
+		barCodeLookUp.put(productName, productBarCode);
+	}
+
+	public void addProduct(Product item) {
+		catalog.put(item.getBarCode(), item);
+	}
+
+	public void addSale(Sale sale) {
+		sales.add(sale);
 	}
 
 	public Map<String, Employee> getEmployees() {
@@ -43,4 +71,9 @@ public class MainSystem implements Serializable {
 	public Map<String, Product> getCatalog() {
 		return catalog;
 	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+
 }
