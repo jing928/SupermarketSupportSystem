245b31b46cddd59a0fb1ccce1cb502fda1383c84
Jing Li <lyovse@gmail.com>
2018-09-27 17:37:44 +1000

    Create a menu printer class

---
+++ src/main/java/view/MenuPrinter.java
@@ -0,0 +1,21 @@
+package view;
+
+public class MenuPrinter {
+
+	public static void printMenu(String[] items) {
+		for (int i = 0; i < items.length; i++) {
+			if (i == 0) {
+				System.out.println(items[i]);
+				System.out.println();
+			} else if (i == items.length - 1) {
+				System.out.println(items[i]);
+			} else {
+				String item = String.format("%-23s %s", items[i], i);
+				System.out.println(item);
+			}
+		}
+
+		System.out.println();
+	}
+	
+}
